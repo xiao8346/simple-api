59fbdbd40dcf938348981b2bb2b4e0cc3d86dbba
Xiao_Lin <Xiao_Lin@compal.com>
2021-01-18 14:27:39 +0800

    feat: export express app to app.ts

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import { STATUS_CODES } from 'http';
+
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as mongoose from 'mongoose';
+import * as Q from 'q';
+import * as morgan from 'morgan';
+
+import { modelsFactory } from './models';
+import { routesFactorey } from './routes';
+import { config } from './config';
+
+export const app = express();
+(mongoose as any).Promise = Q.Promise;
+const conn = mongoose.createConnection('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.set('config', config);
+
+app.set('models', modelsFactory(conn));
+
+app.use(routesFactorey(app));
+
+app.use(function (err, req, res, next) {
+  err.status = err.status || 500;
+  err.message = STATUS_CODES[err.status];
+  res.status(err.status);
+  res.json(err);
+});
+
+const port = process.env.PORT || 3300;
--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,9 @@
-import { STATUS_CODES } from 'http';
+import * as http from 'http';
 
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
-import * as mongoose from 'mongoose';
-import * as Q from 'q';
-import * as morgan from 'morgan';
-
-import { modelsFactory } from './models';
-import { routesFactorey } from './routes';
-import { config } from './config';
-
-const app = express();
-(mongoose as any).Promise = Q.Promise;
-const conn = mongoose.createConnection('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(morgan('tiny'));
-app.set('config', config);
-
-app.set('models', modelsFactory(conn));
-
-app.use(routesFactorey(app));
-
-app.use(function (err, req, res, next) {
-  err.status = err.status || 500;
-  err.message = STATUS_CODES[err.status];
-  res.status(err.status);
-  res.json(err);
-});
+import { app } from './app';
 
 const port = process.env.PORT || 3300;
 
-app.listen(port, () => {
+http.createServer(app).listen(port, () => {
   console.log('listen 3300 port');
-});
+})
